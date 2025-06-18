71f9204d6518188933f8da1d5421e60739ccf695
mrccdric27 <cubecore27@gmail.com>
2025-06-18 13:02:48 +0800

    collect staitic

--- mockapi/start.sh
+++ mockapi/start.sh
@@ -6,6 +6,6 @@ python manage.py migrate
 # Then seed data
 python manage.py seed_projects
 python manage.py seed_checkouts
-
+python manage.py collectstatic --noinput
 # Then start the server
 gunicorn mockapi.wsgi:application --bind 0.0.0.0:8000
